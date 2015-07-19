8dc06f10457bbf61c9bc026179fb44f87c3adb3a
Romero Malaquias <romero.malaquias@gmail.com>
2015-07-19 02:20:58 -0300

    Avaliador funcional

---
+++ src/main/groovy/br/ufal/ic/Evaluator.groovy
@@ -0,0 +1,55 @@
+package br.ufal.ic
+
+import br.ufal.ic.commons.ServerRPC
+import org.codehaus.jackson.map.ObjectMapper
+
+import javax.script.ScriptContext
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+import javax.script.SimpleScriptContext
+
+
+class Evaluator extends ServerRPC {
+    Evaluator(String exchangeName, String key) {
+        super(exchangeName, key)
+    }
+
+    String doWork (String message){
+        Submission submission =  new ObjectMapper().readValue(message, Submission.class);
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("groovy");
+        StringWriter writer = new StringWriter(); //ouput will be stored here
+
+
+        ScriptContext context = new SimpleScriptContext();
+        context.setWriter(writer); //configures output redirection
+
+        try {
+            engine.eval(submission.getCode(), context);
+            String result = writer.toString()
+            if (result.equals(submission.getOutput())) {
+                return "CORRECT"
+            } else {
+                return "WRONG_ANSWER"
+            }
+        } catch (Exception e) {
+            e.printStackTrace()
+            return "COMPILATION_ERROR"
+        }
+
+    }
+
+    public static void main(String[] argv) {
+        Evaluator evaluatorServer = new Evaluator("EXCHANGE", "evaluator");
+
+
+    }
+
+
+}
+
+
+
+
+
+
---
+++ src/main/groovy/br/ufal/ic/SubmissionTest.groovy
@@ -0,0 +1,37 @@
+package br.ufal.ic
+
+import br.ufal.ic.commons.ClientRPC
+
+import javax.script.ScriptContext
+import javax.script.ScriptEngine
+import javax.script.ScriptEngineManager
+import javax.script.SimpleScriptContext
+
+/**
+ * Created by huxley on 18/07/15.
+ */
+class SubmissionTest extends ClientRPC {
+    SubmissionTest(String exchangeName, String key) {
+        super(exchangeName, key);
+    }
+
+    void doWork (String message){
+        println "ok";
+    }
+
+    public static void main(String[] argv) {
+        SubmissionTest fibonacciRpc = new SubmissionTest("EXCHANGE", "evaluator");
+        fibonacciRpc.start();
+        fibonacciRpc.call('{"code":"print \'Ola mundo\';", "language": "groovy", "input": "2", "output": "oi"}');
+        fibonacciRpc.call('{"code":"print(\'oi\');", "language": "groovy", "input": "2", "output": "3"}');
+        fibonacciRpc.call('{"code":"print(\'Ola mundo\');", "language": "groovy", "input": "2", "output": "Ola mundo"}');
+        fibonacciRpc.call('{"code":" \'Ola mundo\';", "language": "groovy", "input": "2", "output": "Ola mundo"}');
+
+    }
+
+}
+
+
+
+
+
